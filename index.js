47285ea33ab6c47206e74ea99d1c7ac2b7018b26
JoshuaJeyaranjan <141107926+JoshuaJeyaranjan@users.noreply.github.com>
2025-10-04 15:49:56 -0400

    inital setup db connect

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const shelterRoutes = require("./routes/shelters");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Routes
+app.use("/shelters", shelterRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Toronto Shelters API is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
