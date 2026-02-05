28d2951128c0dd5b856031bb81a659c1ae5b0199
JoshuaJeyaranjan <141107926+JoshuaJeyaranjan@users.noreply.github.com>
2026-02-05 15:14:41 -0500

    debug db connection issue

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const { Pool } = require("pg");
-
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // use the Render external URL
+
+  connectionString: process.env.DATABASE_URL, // use the railway external URL
   ssl: { rejectUnauthorized: false },
 });
 
--- index.js
+++ index.js
@@ -19,9 +19,11 @@ app.use("/locations", shelterRoutes);
 app.get('/debug/db', async (req, res) => {
   try {
     const result = await pool.query('SELECT NOW()');
+    console.log('DATABASE_URL:', process.env.DATABASE_URL);
     res.json(result.rows[0]);
   } catch (err) {
     console.error(err);
+    console.log('DATABASE_URL:', process.env.DATABASE_URL);
     res.status(500).json({ error: 'DB connection failed' });
   }
 });
