520af508dd56b8c3e99959f9c66a59302f358c95
JoshuaJeyaranjan <141107926+JoshuaJeyaranjan@users.noreply.github.com>
2026-02-05 17:31:46 -0500

    update route order

--- routes/shelters.js
+++ routes/shelters.js
@@ -8,10 +8,10 @@ router.get("/", locationsController.getAllLocations);
 // Return data for mapping (lat/lng + basic info)
 router.get("/map", locationsController.getLocationsForMap);
 
-// Route for a single location's full info
-router.get("/:id", locationsController.getLocationById);
-
 // Occupancy info per location
 router.get("/:id/occupancy", locationsController.getLocationOccupancy);
+
+// Route for a single location's full info
+router.get("/:id", locationsController.getLocationById);
 
 module.exports = router;
